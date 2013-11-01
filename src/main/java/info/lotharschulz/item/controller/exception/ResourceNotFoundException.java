6652f1c713bb0875797bb3bc102bd8da064c8b59
lothar <mail@lotharschulz.info>
2013-11-02 00:21:29 +0100

    serialVersionUID adapted

--- src/main/java/info/lotharschulz/item/controller/exception/ResourceNotFoundException.java
+++ src/main/java/info/lotharschulz/item/controller/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus( value = HttpStatus.NOT_FOUND )
 public class ResourceNotFoundException extends RuntimeException{
 
-    private static final long serialVersionUID = -5562012974937511663L;
+    private static final long serialVersionUID = -556201974937511663L;
 
     public ResourceNotFoundException() {
         super();
